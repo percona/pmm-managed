d569c8b47f728f2a4d14af8d87cf6be205bf0629
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2018-12-10 19:18:41 +0300

    PMM-3314 Test everything.

--- services/agents/channel_test.go
+++ services/agents/channel_test.go
@@ -18,6 +18,7 @@ package agents
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"net"
 	"strings"
@@ -81,6 +82,8 @@ func setup(t *testing.T, connect func(*Channel) error, expected ...error) (agent
 	require.NoError(t, err, "failed to create stream")
 
 	teardown := func(t *testing.T) {
+		require.NotNil(t, channel, "Test exited before first message reached connect handler.")
+
 		err := channel.Wait()
 		assert.Contains(t, expected, errors.Cause(err), "%+v", err)
 
@@ -288,78 +291,66 @@ func TestAgentClosesStream(t *testing.T) {
 	assert.NoError(t, err)
 }
 
-/*
 func TestAgentClosesConnection(t *testing.T) {
-	connect := func(stream agent.Agent_ConnectServer) error { //nolint:unparam
-		err := stream.Send(&agent.ServerMessage{
-			Id: 1,
-			Payload: &agent.ServerMessage_Ping{
-				Ping: new(agent.PingRequest),
-			},
+	connect := func(ch *Channel) error { //nolint:unparam
+		msg := ch.SendRequest(&agent.ServerMessage_Ping{
+			Ping: new(agent.PingRequest),
 		})
-		assert.NoError(t, err)
+		assert.Nil(t, msg)
 
-		msg, err := stream.Recv()
-		assert.Equal(t, status.Error(codes.Canceled, context.Canceled.Error()), err)
+		assert.Nil(t, <-ch.Requests())
+		return nil
+	}
+
+	stream, cc, teardown := setup(t, connect, status.Error(codes.Canceled, context.Canceled.Error()))
+	defer teardown(t)
+
+	msg, err := stream.Recv()
+	require.NoError(t, err)
+	assert.NotNil(t, msg)
+
+	err = cc.Close()
+	assert.NoError(t, err)
+}
+
+func TestUnexpectedResponseFromAgent(t *testing.T) {
+	connect := func(ch *Channel) error { //nolint:unparam
+		// after receiving unexpected response, channel is closed
+		msg := ch.SendRequest(&agent.ServerMessage_Ping{
+			Ping: new(agent.PingRequest),
+		})
+		assert.Nil(t, msg)
+
+		// future requests are ignored
+		msg = ch.SendRequest(&agent.ServerMessage_Ping{
+			Ping: new(agent.PingRequest),
+		})
 		assert.Nil(t, msg)
 
 		return nil
 	}
 
-	// gRPC library has a race in that case, so we can get two errors
-	errClientConnClosing := status.Error(codes.Canceled, "grpc: the client connection is closing") // == grpc.ErrClientConnClosing
-	errConnClosing := status.Error(codes.Unavailable, "transport is closing")
-	channel, cc, teardown := setup(t, connect, errClientConnClosing, errConnClosing)
+	stream, _, teardown := setup(t, connect, fmt.Errorf("no subscriber for ID 111"))
 	defer teardown(t)
 
-	req := <-channel.Requests()
-	ping := req.GetPing()
-	assert.NotNil(t, ping)
+	// this message triggers "no subscriber for ID" error
+	err := stream.Send(&agent.AgentMessage{
+		Id: 111,
+		Payload: &agent.AgentMessage_Ping{
+			Ping: new(agent.PingResponse),
+		},
+	})
+	assert.NoError(t, err)
 
-	err := cc.Close()
+	// this message should not trigger new error
+	err = stream.Send(&agent.AgentMessage{
+		Id: 222,
+		Payload: &agent.AgentMessage_Ping{
+			Ping: new(agent.PingResponse),
+		},
+	})
+	assert.NoError(t, err)
+
+	_, err = stream.Recv()
 	assert.NoError(t, err)
 }
-
-func TestUnexpectedMessageFromServer(t *testing.T) {
-	connect := func(stream agent.Agent_ConnectServer) error { //nolint:unparam
-		// this message triggers "no subscriber for ID" error
-		err := stream.Send(&agent.ServerMessage{
-			Id: 111,
-			Payload: &agent.ServerMessage_QanData{
-				QanData: new(agent.QANDataResponse),
-			},
-		})
-		assert.NoError(t, err)
-
-		// this message should not trigger new error
-		err = stream.Send(&agent.ServerMessage{
-			Id: 222,
-			Payload: &agent.ServerMessage_QanData{
-				QanData: new(agent.QANDataResponse),
-			},
-		})
-		assert.NoError(t, err)
-
-		return nil
-	}
-
-	channel, _, teardown := setup(t, connect, fmt.Errorf("no subscriber for ID 111"))
-	defer teardown(t)
-
-	// after receiving unexpected response, channel is closed
-	resp := channel.SendRequest(&agent.AgentMessage_QanData{
-		QanData: new(agent.QANDataRequest),
-	})
-	assert.Nil(t, resp)
-	msg := <-channel.Requests()
-	assert.Nil(t, msg)
-
-	// future requests are ignored
-	resp = channel.SendRequest(&agent.AgentMessage_QanData{
-		QanData: new(agent.QANDataRequest),
-	})
-	assert.Nil(t, resp)
-	msg = <-channel.Requests()
-	assert.Nil(t, msg)
-}
-*/
