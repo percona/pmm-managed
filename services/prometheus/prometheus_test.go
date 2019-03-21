1d491c56650f52ac86a61fcbaa09a530425aa758
Alexey Palazhchenko <alexey.palazhchenko@percona.com>
2019-03-21 14:15:22 +0300

    PMM-3689 Add integration test.

--- services/prometheus/prometheus_test.go
+++ services/prometheus/prometheus_test.go
@@ -18,42 +18,186 @@ package prometheus
 
 import (
 	"context"
+	"database/sql"
 	"io/ioutil"
 	"path/filepath"
 	"testing"
 
+	"github.com/AlekSi/pointer"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"gopkg.in/reform.v1"
 	"gopkg.in/reform.v1/dialects/postgresql"
 
+	"github.com/percona/pmm-managed/models"
 	"github.com/percona/pmm-managed/utils/logger"
 	"github.com/percona/pmm-managed/utils/tests"
 )
 
-func TestDefaultConfig(t *testing.T) {
-	sqlDB := tests.OpenTestDB(t)
-	defer func() {
-		require.NoError(t, sqlDB.Close())
-	}()
-	db := reform.NewDB(sqlDB, postgresql.Dialect, reform.NewPrintfLogger(t.Logf))
+var configPath = filepath.Join("..", "..", "testdata", "prometheus", "prometheus.yml")
 
-	// always restore original file after test
-	configPath := filepath.Join("..", "..", "testdata", "prometheus", "prometheus.yml")
-	original, err := ioutil.ReadFile(configPath) //nolint:gosec
-	require.NoError(t, err)
-	defer func() {
-		require.NoError(t, ioutil.WriteFile(configPath, original, 0644))
-	}()
+func setup(t *testing.T) (context.Context, *reform.DB, *Service, []byte) {
+	t.Helper()
 
 	ctx := logger.Set(context.Background(), t.Name())
+
+	sqlDB := tests.OpenTestDB(t)
+	db := reform.NewDB(sqlDB, postgresql.Dialect, reform.NewPrintfLogger(t.Logf))
+
 	svc, err := NewService(configPath, "promtool", db, "http://127.0.0.1:9090/prometheus/")
 	require.NoError(t, err)
 	require.NoError(t, svc.Check(ctx))
 
-	assert.NoError(t, svc.UpdateConfiguration(ctx))
-
-	b, err := ioutil.ReadFile(configPath) //nolint:gosec
+	original, err := ioutil.ReadFile(configPath) //nolint:gosec
 	require.NoError(t, err)
-	assert.Equal(t, string(original), string(b))
+
+	return ctx, db, svc, original
+}
+
+func teardown(t *testing.T, ctx context.Context, db *reform.DB, svc *Service, original []byte) { //nolint:golint
+	t.Helper()
+
+	assert.NoError(t, ioutil.WriteFile(configPath, original, 0644))
+	assert.NoError(t, svc.reload())
+
+	assert.NoError(t, db.DBInterface().(*sql.DB).Close())
+}
+
+func TestPrometheus(t *testing.T) {
+	t.Run("Default", func(t *testing.T) {
+		ctx, db, svc, original := setup(t)
+		defer teardown(t, ctx, db, svc, original)
+
+		assert.NoError(t, svc.UpdateConfiguration(ctx))
+
+		actual, err := ioutil.ReadFile(configPath) //nolint:gosec
+		require.NoError(t, err)
+		assert.Equal(t, string(original), string(actual))
+	})
+
+	t.Run("Normal", func(t *testing.T) {
+		ctx, db, svc, original := setup(t)
+		defer teardown(t, ctx, db, svc, original)
+
+		for _, str := range []reform.Struct{
+			&models.Node{
+				NodeID:       "/node_id/cc663f36-18ca-40a1-aea9-c6310bb4738d",
+				NodeType:     models.GenericNodeType,
+				NodeName:     "test-generic-node",
+				Address:      pointer.ToString("1.2.3.4"),
+				CustomLabels: []byte(`{"_node_label": "foo"}`),
+			},
+			&models.Service{
+				ServiceID:    "/service_id/014647c3-b2f5-44eb-94f4-d943260a968c",
+				ServiceType:  models.MySQLServiceType,
+				ServiceName:  "test-mysql",
+				NodeID:       "/node_id/cc663f36-18ca-40a1-aea9-c6310bb4738d",
+				Address:      pointer.ToString("5.6.7.8"),
+				CustomLabels: []byte(`{"_service_label": "bar"}`),
+			},
+			&models.Agent{
+				AgentID:      "/agent_id/75bb30d3-ef4a-4147-97a8-621a996611dd",
+				AgentType:    models.MySQLdExporterType,
+				CustomLabels: []byte(`{"_agent_label": "baz"}`),
+				ListenPort:   pointer.ToUint16(12345),
+			},
+			&models.AgentService{
+				AgentID:   "/agent_id/75bb30d3-ef4a-4147-97a8-621a996611dd",
+				ServiceID: "/service_id/014647c3-b2f5-44eb-94f4-d943260a968c",
+			},
+		} {
+			require.NoError(t, db.Insert(str))
+		}
+
+		assert.NoError(t, svc.UpdateConfiguration(ctx))
+
+		expected := `# Managed by pmm-managed. DO NOT EDIT.
+---
+global:
+  scrape_interval: 1m
+  scrape_timeout: 10s
+  evaluation_interval: 1m
+rule_files:
+- /etc/prometheus.d/*.rules.yml
+scrape_configs:
+- job_name: prometheus
+  scrape_interval: 1s
+  scrape_timeout: 1s
+  metrics_path: /prometheus/metrics
+  static_configs:
+  - targets:
+    - 127.0.0.1:9090
+    labels:
+      instance: pmm-server
+- job_name: grafana
+  scrape_interval: 5s
+  scrape_timeout: 4s
+  metrics_path: /metrics
+  static_configs:
+  - targets:
+    - 127.0.0.1:3000
+    labels:
+      instance: pmm-server
+- job_name: pmm-managed
+  scrape_interval: 10s
+  scrape_timeout: 5s
+  metrics_path: /debug/metrics
+  static_configs:
+  - targets:
+    - 127.0.0.1:7773
+    labels:
+      instance: pmm-server
+- job_name: mysqld_exporter_agent_id_75bb30d3-ef4a-4147-97a8-621a996611dd_hr
+  scrape_interval: 1s
+  scrape_timeout: 1s
+  metrics_path: /metrics-hr
+  static_configs:
+  - targets:
+    - 1.2.3.4:12345
+    labels:
+      _agent_label: baz
+      _node_label: foo
+      _service_label: bar
+      instance: /agent_id/75bb30d3-ef4a-4147-97a8-621a996611dd
+      node_id: /node_id/cc663f36-18ca-40a1-aea9-c6310bb4738d
+      node_name: test-generic-node
+      service_id: /service_id/014647c3-b2f5-44eb-94f4-d943260a968c
+      service_name: test-mysql
+- job_name: mysqld_exporter_agent_id_75bb30d3-ef4a-4147-97a8-621a996611dd_mr
+  scrape_interval: 10s
+  scrape_timeout: 5s
+  metrics_path: /metrics-mr
+  static_configs:
+  - targets:
+    - 1.2.3.4:12345
+    labels:
+      _agent_label: baz
+      _node_label: foo
+      _service_label: bar
+      instance: /agent_id/75bb30d3-ef4a-4147-97a8-621a996611dd
+      node_id: /node_id/cc663f36-18ca-40a1-aea9-c6310bb4738d
+      node_name: test-generic-node
+      service_id: /service_id/014647c3-b2f5-44eb-94f4-d943260a968c
+      service_name: test-mysql
+- job_name: mysqld_exporter_agent_id_75bb30d3-ef4a-4147-97a8-621a996611dd_lr
+  scrape_interval: 1m
+  scrape_timeout: 10s
+  metrics_path: /metrics-lr
+  static_configs:
+  - targets:
+    - 1.2.3.4:12345
+    labels:
+      _agent_label: baz
+      _node_label: foo
+      _service_label: bar
+      instance: /agent_id/75bb30d3-ef4a-4147-97a8-621a996611dd
+      node_id: /node_id/cc663f36-18ca-40a1-aea9-c6310bb4738d
+      node_name: test-generic-node
+      service_id: /service_id/014647c3-b2f5-44eb-94f4-d943260a968c
+      service_name: test-mysql
+`
+		actual, err := ioutil.ReadFile(configPath) //nolint:gosec
+		require.NoError(t, err)
+		assert.Equal(t, expected, string(actual))
+	})
 }
