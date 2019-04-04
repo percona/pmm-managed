d836b8af91bd461dcd1143ece082d592127289a8
Anton Kucherov <anton.kucherov@percona.com>
2019-04-04 15:35:34 +0300

    PMM-3541 Fix error handling in register node api.

--- services/management/node.go
+++ services/management/node.go
@@ -22,12 +22,18 @@ import (
 	"github.com/AlekSi/pointer"
 	inventorypb "github.com/percona/pmm/api/inventory"
 	"github.com/percona/pmm/api/managementpb"
+	"github.com/pkg/errors"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"gopkg.in/reform.v1"
 
 	"github.com/percona/pmm-managed/models"
 	"github.com/percona/pmm-managed/services/inventory" // TODO: Refactor, as service shouldn't depend on other service in one abstraction level.
+)
+
+var (
+	errNodeNotFound  = errors.New("node not found")
+	errAgentNotFound = errors.New("agent not found")
 )
 
 // NodeService represents service for working with nodes.
@@ -51,19 +57,18 @@ func (s *NodeService) Register(ctx context.Context, req *managementpb.RegisterNo
 	if e := s.db.InTransaction(func(tx *reform.TX) error {
 
 		node, err := s.findNodeByName(ctx, tx.Querier, req.NodeName)
-		switch err.(type) {
-		case nodeNotFoundErr:
-			node, err = s.createNewNode(ctx, tx.Querier, req)
-			if err != nil {
-				return err
-			}
+		switch err {
 		case nil:
 			params := &models.UpdateNodeParams{MachineID: req.MachineId, CustomLabels: req.CustomLabels}
 			node, err = models.UpdateNode(tx.Querier, node.NodeID, params)
 			if err != nil {
 				return err
 			}
-
+		case errNodeNotFound:
+			node, err = s.createNewNode(ctx, tx.Querier, req)
+			if err != nil {
+				return err
+			}
 		default:
 			return err
 		}
@@ -73,8 +78,8 @@ func (s *NodeService) Register(ctx context.Context, req *managementpb.RegisterNo
 		}
 
 		pmmAgent, err := s.findPmmAgentByNodeID(ctx, tx.Querier, node.NodeID)
-		switch err.(type) {
-		case agentNotFoundErr:
+		switch err {
+		case errAgentNotFound:
 			pmmAgent, err = models.AgentAddPmmAgent(tx.Querier, node.NodeID, nil)
 			if err != nil {
 				return err
@@ -90,8 +95,8 @@ func (s *NodeService) Register(ctx context.Context, req *managementpb.RegisterNo
 		}
 
 		_, err = s.findNodeExporterByPmmAgentID(ctx, tx.Querier, pmmAgent.AgentID)
-		switch err.(type) {
-		case agentNotFoundErr:
+		switch err {
+		case errAgentNotFound:
 			_, err := models.AgentAddNodeExporter(tx.Querier, pmmAgent.AgentID, nil)
 			if err != nil {
 				return err
@@ -154,8 +159,7 @@ func (s *NodeService) findNodeByName(ctx context.Context, q *reform.Querier, nam
 		}
 	}
 
-	var nfErr nodeNotFoundErr = "node not found"
-	return nil, nfErr
+	return nil, errNodeNotFound
 }
 
 func (s *NodeService) findPmmAgentByNodeID(ctx context.Context, q *reform.Querier, nodeID string) (pmmAgent *models.Agent, err error) {
@@ -170,8 +174,7 @@ func (s *NodeService) findPmmAgentByNodeID(ctx context.Context, q *reform.Querie
 		}
 	}
 
-	var anfErr agentNotFoundErr = "agent not found"
-	return pmmAgent, anfErr
+	return pmmAgent, errAgentNotFound
 }
 
 func (s *NodeService) findNodeExporterByPmmAgentID(ctx context.Context, q *reform.Querier, pmmAgentID string) (nodeExporter *inventorypb.NodeExporter, err error) {
@@ -191,8 +194,7 @@ func (s *NodeService) findNodeExporterByPmmAgentID(ctx context.Context, q *refor
 		}
 	}
 
-	var anfErr agentNotFoundErr = "agent not found"
-	return nodeExporter, anfErr
+	return nodeExporter, errAgentNotFound
 }
 
 func (s *NodeService) addNodeToResponse(model *models.Node, res *managementpb.RegisterNodeResponse) error {
@@ -218,16 +220,4 @@ func (s *NodeService) addPmmAgentToResponse(q *reform.Querier, model *models.Age
 	}
 	res.PmmAgent = invAgent.(*inventorypb.PMMAgent)
 	return nil
-}
-
-type nodeNotFoundErr string
-
-func (e nodeNotFoundErr) Error() string {
-	return string(e)
-}
-
-type agentNotFoundErr string
-
-func (e agentNotFoundErr) Error() string {
-	return string(e)
 }
