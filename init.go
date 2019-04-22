1a5e0d18897927a0a9cb8a55774691a34fc5bc9b
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2019-04-22 15:27:39 +0300

    Update Swagger client settings. (#11)
    * Handle nginx responses when pmm-managed is down.
    * Fail if username is given.
    * Fix TLS certificate check.
    * Allow to disable TLS certificate check.

--- init.go
+++ init.go
@@ -4,12 +4,17 @@ import (
 	"context"
 	"crypto/tls"
 	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
 	"net/http"
 	"net/url"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"github.com/go-openapi/runtime"
 	httptransport "github.com/go-openapi/runtime/client"
 	"github.com/percona/pmm/api/inventorypb/json/client"
 	"github.com/sirupsen/logrus"
@@ -28,16 +33,28 @@ var (
 	Hostname string
 )
 
+type errFromNginx string
+
+func (e errFromNginx) Error() string {
+	return "response from nginx: " + string(e)
+}
+
+func (e errFromNginx) GoString() string {
+	return fmt.Sprintf("errFromNginx(%q)", string(e))
+}
+
 //nolint:gochecknoinits
 func init() {
 	debugF := flag.Bool("pmm.debug", false, "Enable debug output [PMM_DEBUG].")
 	traceF := flag.Bool("pmm.trace", false, "Enable trace output [PMM_TRACE].")
 	serverURLF := flag.String("pmm.server-url", "https://127.0.0.1:8443/", "PMM Server URL [PMM_SERVER_URL].")
+	serverInsecureTLSF := flag.Bool("pmm.server-insecure-tls", false, "Skip PMM Server TLS certificate validation.")
 	flag.Parse()
 	envvars := map[string]*flag.Flag{
-		"PMM_DEBUG":      flag.Lookup("pmm.debug"),
-		"PMM_TRACE":      flag.Lookup("pmm.trace"),
-		"PMM_SERVER_URL": flag.Lookup("pmm.server-url"),
+		"PMM_DEBUG":               flag.Lookup("pmm.debug"),
+		"PMM_TRACE":               flag.Lookup("pmm.trace"),
+		"PMM_SERVER_URL":          flag.Lookup("pmm.server-url"),
+		"PMM_SERVER_INSECURE_TLS": flag.Lookup("pmm.server-insecure-tls"),
 	}
 
 	for envVar, f := range envvars {
@@ -91,9 +108,46 @@ func init() {
 
 	// use JSON APIs over HTTP/1.1
 	transport := httptransport.New(BaseURL.Host, BaseURL.Path, []string{BaseURL.Scheme})
+	// FIXME https://jira.percona.com/browse/PMM-3867
+	if BaseURL.User != nil {
+		logrus.Panic("PMM Server authentication is not implemented yet.")
+	}
 	transport.SetLogger(logrus.WithField("component", "client"))
-	transport.SetDebug(*debugF || *traceF)
-	// disable HTTP/2
-	transport.Transport.(*http.Transport).TLSNextProto = map[string]func(string, *tls.Conn) http.RoundTripper{}
+	transport.SetDebug(logrus.GetLevel() >= logrus.DebugLevel)
+	transport.Context = context.Background() // not Context - do not cancel the whole transport
+
+	// set error handlers for nginx responses if pmm-managed is down
+	errorConsumer := runtime.ConsumerFunc(func(reader io.Reader, data interface{}) error {
+		b, _ := ioutil.ReadAll(reader)
+		return errFromNginx(string(b))
+	})
+	transport.Consumers = map[string]runtime.Consumer{
+		runtime.JSONMime:    runtime.JSONConsumer(),
+		runtime.HTMLMime:    errorConsumer,
+		runtime.TextMime:    errorConsumer,
+		runtime.DefaultMime: errorConsumer,
+	}
+
+	// disable HTTP/2, set TLS config
+	httpTransport := transport.Transport.(*http.Transport)
+	httpTransport.TLSNextProto = map[string]func(string, *tls.Conn) http.RoundTripper{}
+	if BaseURL.Scheme == "https" {
+		if httpTransport.TLSClientConfig == nil {
+			httpTransport.TLSClientConfig = new(tls.Config)
+		}
+		if *serverInsecureTLSF {
+			httpTransport.TLSClientConfig.InsecureSkipVerify = true
+		} else {
+			host, _, _ := net.SplitHostPort(BaseURL.Host)
+			httpTransport.TLSClientConfig.ServerName = host
+		}
+	}
+
 	client.Default = client.New(transport, nil)
 }
+
+// check interfaces
+var (
+	_ error          = errFromNginx("")
+	_ fmt.GoStringer = errFromNginx("")
+)
