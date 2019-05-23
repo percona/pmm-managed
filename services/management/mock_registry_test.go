164447003361d5234f864ac3b7581449d134cc85
Anton Kucherov <anton.kucherov@percona.com>
2019-05-23 13:27:43 +0300

    PMM-3832 Update deps.

--- services/management/mock_registry_test.go
+++ services/management/mock_registry_test.go
@@ -2,7 +2,6 @@
 
 package management
 
-import agentpb "github.com/percona/pmm/api/agentpb"
 import context "context"
 import mock "github.com/stretchr/testify/mock"
 
@@ -28,29 +27,6 @@ func (_m *mockRegistry) IsConnected(pmmAgentID string) bool {
 // Kick provides a mock function with given fields: ctx, pmmAgentID
 func (_m *mockRegistry) Kick(ctx context.Context, pmmAgentID string) {
 	_m.Called(ctx, pmmAgentID)
-}
-
-// SendRequest provides a mock function with given fields: ctx, pmmAgentID, payload
-func (_m *mockRegistry) SendRequest(ctx context.Context, pmmAgentID string, payload agentpb.ServerRequestPayload) (agentpb.AgentResponsePayload, error) {
-	ret := _m.Called(ctx, pmmAgentID, payload)
-
-	var r0 agentpb.AgentResponsePayload
-	if rf, ok := ret.Get(0).(func(context.Context, string, agentpb.ServerRequestPayload) agentpb.AgentResponsePayload); ok {
-		r0 = rf(ctx, pmmAgentID, payload)
-	} else {
-		if ret.Get(0) != nil {
-			r0 = ret.Get(0).(agentpb.AgentResponsePayload)
-		}
-	}
-
-	var r1 error
-	if rf, ok := ret.Get(1).(func(context.Context, string, agentpb.ServerRequestPayload) error); ok {
-		r1 = rf(ctx, pmmAgentID, payload)
-	} else {
-		r1 = ret.Error(1)
-	}
-
-	return r0, r1
 }
 
 // SendSetStateRequest provides a mock function with given fields: ctx, pmmAgentID
