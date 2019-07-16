c803b3846a5c1a34d636d17276c52f3013592940
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2019-07-16 16:59:31 +0300

    Update version test.

--- server/version_test.go
+++ server/version_test.go
@@ -25,6 +25,7 @@ func TestVersion(t *testing.T) {
 				Path: path,
 			})
 
+			t.Logf("URI: %s", uri)
 			resp, err := http.Get(uri.String())
 			require.NoError(t, err)
 			defer resp.Body.Close() //nolint:errcheck
@@ -36,12 +37,7 @@ func TestVersion(t *testing.T) {
 			err = json.NewDecoder(resp.Body).Decode(&res)
 			require.NoError(t, err)
 
-			assert.True(t, strings.HasPrefix(res.Version, "2.0.0"), "version = %q", res.Version)
-
-			// TODO https://jira.percona.com/browse/PMM-4160
-			// tt := pmmapitests.ExpectFailure(t, "https://jira.percona.com/browse/PMM-4114")
-			// defer tt.Check()
-			// assert.True(tt, strings.HasPrefix(res.Version, "2.0.0-"), "version = %q should has suffix", res.Version)
+			assert.True(t, strings.HasPrefix(res.Version, "2.0.0-"), "version = %q should has suffix", res.Version)
 		})
 	}
 }
