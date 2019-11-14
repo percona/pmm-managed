6e9456c5d355b94dfe9887f9d7d111698de07215
Carlos Salguero <carlos.salguero@percona.com>
2019-11-14 18:20:18 -0300

    PMM-4475 Added integration test

--- services/management/rds_discovery_test.go
+++ services/management/rds_discovery_test.go
@@ -16,6 +16,7 @@ import (
 	"github.com/percona/pmm-managed/utils/testdb"
 	"github.com/percona/pmm-managed/utils/tests"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"gopkg.in/reform.v1"
 	"gopkg.in/reform.v1/dialects/postgresql"
 )
@@ -30,10 +31,9 @@ func (m *mockRDSClient) DescribeDBInstancesPagesWithContext(ctx context.Context,
 		fn(getMockedInstances(), true)
 	}
 	return nil
-
 }
 
-func TestRdsServiceDiscovery(t *testing.T) {
+func TestRdsServiceDiscoveryUnit(t *testing.T) {
 	ctx := logger.Set(context.Background(), t.Name())
 
 	sqlDB := testdb.Open(t, models.SetupFixtures)
@@ -61,6 +61,29 @@ func TestRdsServiceDiscovery(t *testing.T) {
 	instances, err := rds.Discover(ctx, accessKey, secretKey)
 	assert.NoError(t, err)
 	assert.Equal(t, len(instances.RdsInstances), 1)
+	require.NoError(t, sqlDB.Close())
+}
+
+func TestRdsServiceDiscoveryIntegration(t *testing.T) {
+	ctx := logger.Set(context.Background(), t.Name())
+
+	sqlDB := testdb.Open(t, models.SetupFixtures)
+	db := reform.NewDB(sqlDB, postgresql.Dialect, reform.NewPrintfLogger(t.Logf))
+
+	r := new(mockAgentsRegistry)
+	r.Test(t)
+
+	accessKey, secretKey := tests.GetAWSKeys(t)
+	rds := NewRDSService(db, r)
+
+	instances, err := rds.Discover(ctx, accessKey, secretKey)
+
+	//TODO: Improve this test.
+	// In our current testing env with current AWS keys, 2 regions are returning errors but we don't know why for sure
+	// Also, probably we can have more than 1 instance or none. PLEASE UPDATE THIS TESTS !
+	assert.NotNil(t, err)
+	assert.GreaterOrEqualf(t, len(instances.RdsInstances), 1, "Should have at least one instance")
+	require.NoError(t, sqlDB.Close())
 }
 
 func getMockedInstances() *rds.DescribeDBInstancesOutput {
