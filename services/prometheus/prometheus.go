104b60cdb3792748f91be9a00da74d1c72a3dc70
Andrii Skomorokhov <andrii.skomorokhov@percona.com>
2019-12-24 11:26:51 +0200

    PMM-4086 Add qan-api to prometheus config.

--- services/prometheus/prometheus.go
+++ services/prometheus/prometheus.go
@@ -150,6 +150,7 @@ func (svc *Service) marshalConfig() ([]byte, error) {
 				scrapeConfigForPrometheus(s.HR),
 				scrapeConfigForGrafana(s.MR),
 				scrapeConfigForPMMManaged(s.MR),
+				scrapeConfigForQANAPI2(s.MR),
 			},
 		}
 
