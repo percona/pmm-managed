40cba81c049924f8f2e1de57750659fa853ea52d
Artem Gavrilov <artem.gavrilov@percona.com>
2020-02-25 12:21:37 +0200

    PMM-3387 Fix comment

--- services/prometheus/internal/prometheus/discovery/config/config.go
+++ services/prometheus/internal/prometheus/discovery/config/config.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-// Original file: https://github.com/prometheus/prometheus/blob/v2.7.1/discovery/config/config.go
+// Original file: https://github.com/prometheus/prometheus/blob/v2.12.0/discovery/config/config.go
 // Only static_configs were kept, everything else was removed.
 
 // Copyright 2016 The Prometheus Authors
