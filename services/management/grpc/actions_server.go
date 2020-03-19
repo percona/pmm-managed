69666fa22d2f5f39ed89e64c4febf947f326ddd4
Alexey Palazhchenko <alexey.palazhchenko@percona.com>
2020-03-19 20:26:12 +0300

    PMM-4192 Add nolint.

--- services/management/grpc/actions_server.go
+++ services/management/grpc/actions_server.go
@@ -194,6 +194,7 @@ func (s *actionsServer) StartMySQLShowIndexAction(ctx context.Context, req *mana
 }
 
 // StartPostgreSQLShowCreateTableAction starts PostgreSQL SHOW CREATE TABLE Action.
+//nolint:lll
 func (s *actionsServer) StartPostgreSQLShowCreateTableAction(ctx context.Context, req *managementpb.StartPostgreSQLShowCreateTableActionRequest) (*managementpb.StartPostgreSQLShowCreateTableActionResponse, error) {
 	res, dsn, err := s.prepareServiceAction(req.ServiceId, req.PmmAgentId, req.Database)
 	if err != nil {
@@ -212,6 +213,7 @@ func (s *actionsServer) StartPostgreSQLShowCreateTableAction(ctx context.Context
 }
 
 // StartPostgreSQLShowIndexAction starts PostgreSQL SHOW INDEX Action.
+//nolint:lll
 func (s *actionsServer) StartPostgreSQLShowIndexAction(ctx context.Context, req *managementpb.StartPostgreSQLShowIndexActionRequest) (*managementpb.StartPostgreSQLShowIndexActionResponse, error) {
 	res, dsn, err := s.prepareServiceAction(req.ServiceId, req.PmmAgentId, req.Database)
 	if err != nil {
@@ -230,8 +232,8 @@ func (s *actionsServer) StartPostgreSQLShowIndexAction(ctx context.Context, req 
 }
 
 // StartPostgreSQLShowIndexAction starts PostgreSQL SHOW INDEX Action.
-func (s *actionsServer) StartMongoDBExplainAction(ctx context.Context, req *managementpb.StartMongoDBExplainActionRequest) (
-	*managementpb.StartMongoDBExplainActionResponse, error) {
+//nolint:lll
+func (s *actionsServer) StartMongoDBExplainAction(ctx context.Context, req *managementpb.StartMongoDBExplainActionRequest) (*managementpb.StartMongoDBExplainActionResponse, error) {
 	res, dsn, err := s.prepareServiceAction(req.ServiceId, req.PmmAgentId, req.Database)
 	if err != nil {
 		return nil, err
