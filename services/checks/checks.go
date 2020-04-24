2f5ab310e949a1a6f9952988cdf267045baf8fdc
Artem Gavrilov <artem.gavrilov@percona.com>
2020-04-24 15:33:12 +0200

    SAAS-20 Capitalize DB's names in log messages

--- services/checks/checks.go
+++ services/checks/checks.go
@@ -226,19 +226,19 @@ func (s *Service) executeChecks() {
 
 	mySQLRes, err := s.executeMySQLChecks(mySQLChecks)
 	if err != nil {
-		s.l.Errorf("Failed to execute mySQL checks: %s.", err)
+		s.l.Errorf("Failed to execute MySQL checks: %s.", err)
 	}
 	s.addResults(mySQLRes)
 
 	postgreSQLRes, err := s.executePostgreSQLChecks(postgreSQLChecks)
 	if err != nil {
-		s.l.Errorf("Failed to execute postgreSQL checks: %s.", err)
+		s.l.Errorf("Failed to execute PostgreSQL checks: %s.", err)
 	}
 	s.addResults(postgreSQLRes)
 
 	mongoDBRes, err := s.executeMongoChecks(mongoDBChecks)
 	if err != nil {
-		s.l.Errorf("Failed to execute mongoDB checks: %s.", err)
+		s.l.Errorf("Failed to execute MongoDB checks: %s.", err)
 	}
 	s.addResults(mongoDBRes)
 }
@@ -264,16 +264,16 @@ func (s *Service) executeMySQLChecks(checks []check.Check) ([]checkTask, error) 
 			switch c.Type {
 			case check.MySQLShow:
 				if err := s.registry.StartMySQLQueryShowAction(context.Background(), r.ID, pmmAgentID, dsn, c.Query); err != nil {
-					s.l.Errorf("Failed to start mySQL show query action for agent %s, reason: %s.", pmmAgentID, err)
+					s.l.Errorf("Failed to start MySQL show query action for agent %s, reason: %s.", pmmAgentID, err)
 					continue
 				}
 			case check.MySQLSelect:
 				if err := s.registry.StartMySQLQuerySelectAction(context.Background(), r.ID, pmmAgentID, dsn, c.Query); err != nil {
-					s.l.Errorf("Failed to start mySQL select query action for agent %s, reason: %s.", pmmAgentID, err)
+					s.l.Errorf("Failed to start MySQL select query action for agent %s, reason: %s.", pmmAgentID, err)
 					continue
 				}
 			default:
-				s.l.Errorf("Unknown mySQL check type: %s.", c.Type)
+				s.l.Errorf("Unknown MySQL check type: %s.", c.Type)
 				continue
 			}
 
@@ -309,16 +309,16 @@ func (s *Service) executePostgreSQLChecks(checks []check.Check) ([]checkTask, er
 			switch c.Type {
 			case check.PostgreSQLShow:
 				if err := s.registry.StartPostgreSQLQueryShowAction(context.Background(), r.ID, pmmAgentID, dsn); err != nil {
-					s.l.Errorf("Failed to start postgreSQL show query action for agent %s, reason: %s.", pmmAgentID, err)
+					s.l.Errorf("Failed to start PostgreSQL show query action for agent %s, reason: %s.", pmmAgentID, err)
 					continue
 				}
 			case check.PostgreSQLSelect:
 				if err := s.registry.StartPostgreSQLQuerySelectAction(context.Background(), r.ID, pmmAgentID, dsn, c.Query); err != nil {
-					s.l.Errorf("Failed to start postgreSQL select query action for agent %s, reason: %s.", pmmAgentID, err)
+					s.l.Errorf("Failed to start PostgreSQL select query action for agent %s, reason: %s.", pmmAgentID, err)
 					continue
 				}
 			default:
-				s.l.Errorf("Unknown postgresSQL check type: %s.", c.Type)
+				s.l.Errorf("Unknown PostgresSQL check type: %s.", c.Type)
 				continue
 			}
 			res = append(res, checkTask{
@@ -353,17 +353,17 @@ func (s *Service) executeMongoChecks(checks []check.Check) ([]checkTask, error) 
 			switch c.Type {
 			case check.MongoDBGetParameter:
 				if err := s.registry.StartMongoDBQueryGetParameterAction(context.Background(), r.ID, pmmAgentID, dsn); err != nil {
-					s.l.Errorf("Failed to start mongoDB get parameter query action for agent %s, reason: %s.", pmmAgentID, err)
+					s.l.Errorf("Failed to start MongoDB get parameter query action for agent %s, reason: %s.", pmmAgentID, err)
 					continue
 				}
 			case check.MongoDBBuildInfo:
 				if err := s.registry.StartMongoDBQueryBuildInfoAction(context.Background(), r.ID, pmmAgentID, dsn); err != nil {
-					s.l.Errorf("Failed to start mongoDB build info query action for agent %s, reason: %s.", pmmAgentID, err)
+					s.l.Errorf("Failed to start MongoDB build info query action for agent %s, reason: %s.", pmmAgentID, err)
 					continue
 				}
 
 			default:
-				s.l.Errorf("Unknown mongoDB check type: %s.", c.Type)
+				s.l.Errorf("Unknown MongoDB check type: %s.", c.Type)
 				continue
 			}
 			res = append(res, checkTask{
