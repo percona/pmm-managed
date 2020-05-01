a21cd480883d9082b6d5027e8e6d036eca2379c8
Artem Gavrilov <artem.gavrilov@percona.com>
2020-05-01 17:23:48 +0200

    SAAS-30 Remove test code

--- services/alertmanager/alert.go
+++ services/alertmanager/alert.go
@@ -17,12 +17,8 @@
 package alertmanager
 
 import (
-	"fmt"
-
 	"github.com/percona-platform/saas/pkg/check"
-	"github.com/percona/pmm/api/alertmanager/ammodels"
 	"github.com/pkg/errors"
-	"github.com/prometheus/common/model"
 
 	"github.com/percona/pmm-managed/models"
 )
@@ -56,94 +52,4 @@ func (ap *AlertParams) validate() error {
 	}
 
 	return nil
-}
-
-// makeAlert makes alert from given parameters.
-func makeAlert(params *AlertParams) (*ammodels.PostableAlert, error) {
-	if err := params.validate(); err != nil {
-		return nil, err
-	}
-
-	labels, err := models.MergeLabels(params.Node, params.Service, params.Agent)
-	if err != nil {
-		return nil, err
-	}
-
-	labels[model.AlertNameLabel] = params.Name
-	labels["severity"] = params.Severity.String()
-	labels["stt_check"] = "1"
-
-	return &ammodels.PostableAlert{
-		Alert: ammodels.Alert{
-			// GeneratorURL: "TODO",
-			Labels: labels,
-		},
-
-		// StartsAt and EndAt can't be added there without changes in Registry
-
-		Annotations: map[string]string{
-			"summary":     params.Summary,
-			"description": params.Description,
-		},
-	}, nil
-}
-
-// makeAlertPMMAgentNotConnected makes pmm_agent_not_connected alert.
-func makeAlertPMMAgentNotConnected(agent *models.Agent, node *models.Node) (string, *ammodels.PostableAlert, error) {
-	name := "pmm_agent_not_connected"
-	alert, err := makeAlert(&AlertParams{
-		Name:        name,
-		Summary:     "pmm-agent is not connected to PMM Server",
-		Description: fmt.Sprintf("Node name: %s", node.NodeName),
-		Severity:    check.Warning,
-
-		Node:  node,
-		Agent: agent,
-	})
-	if err != nil {
-		return "", nil, err
-	}
-	return name, alert, nil
-}
-
-// makeAlertPMMAgentIsOutdated makes pmm_agent_outdated alert.
-func makeAlertPMMAgentIsOutdated(agent *models.Agent, node *models.Node, serverVersion string) (string, *ammodels.PostableAlert, error) {
-	name := "pmm_agent_outdated"
-	alert, err := makeAlert(&AlertParams{
-		Name:    name,
-		Summary: "pmm-agent is outdated",
-		Description: fmt.Sprintf(
-			"Node name: %s\npmm-agent version: %s\nPMM Server version: %s",
-			node.NodeName, *agent.Version, serverVersion,
-		),
-		Severity: check.Info,
-
-		Node:  node,
-		Agent: agent,
-	})
-	if err != nil {
-		return "", nil, err
-	}
-	return name, alert, nil
-}
-
-// makeAlertPostgreSQLIsOutdated makes postgresql_outdated alert.
-func makeAlertPostgreSQLIsOutdated(node *models.Node, service *models.Service) (string, *ammodels.PostableAlert, error) {
-	name := "postgresql_outdated"
-	alert, err := makeAlert(&AlertParams{
-		Name:    name,
-		Summary: "PostgreSQL is outdated",
-		Description: fmt.Sprintf(
-			"Node name: %s\nCurrent version: %s\nLatest version: %s",
-			node.NodeName, "Fake", "Bogus",
-		),
-		Severity: check.Info,
-
-		Node:    node,
-		Service: service,
-	})
-	if err != nil {
-		return "", nil, err
-	}
-	return name, alert, nil
 }
