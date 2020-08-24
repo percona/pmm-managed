1a653ed7e1c38fab5ba99826b5bc8976cabb94de
Nurlan Moldomurov <nurlan.moldomurov@percona.com>
2020-08-24 15:49:42 +0300

    PMM-6130 Use inner message.

--- services/management/dbaas/kubernetes_server.go
+++ services/management/dbaas/kubernetes_server.go
@@ -42,9 +42,9 @@ func (k kubernetesServer) ListKubernetesClusters(ctx context.Context, _ *dbaasv1
 		return nil, err
 	}
 
-	clusters := make([]*dbaasv1beta1.KubernetesCluster, len(kubernetesClusters))
+	clusters := make([]*dbaasv1beta1.ListKubernetesClustersResponse_KubernetesCluster, len(kubernetesClusters))
 	for i, cluster := range kubernetesClusters {
-		clusters[i] = &dbaasv1beta1.KubernetesCluster{
+		clusters[i] = &dbaasv1beta1.ListKubernetesClustersResponse_KubernetesCluster{
 			KubernetesClusterName: cluster.KubernetesClusterName,
 		}
 	}
