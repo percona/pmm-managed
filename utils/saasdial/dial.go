9f6b3430dfcab68c3eb43372d267751a38387607
Artem Gavrilov <artem.gavrilov@percona.com>
2020-10-07 14:56:43 +0200

    SAAS-320 Fix comment

--- utils/saasdial/dial.go
+++ utils/saasdial/dial.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-// Package dial provides gRPC connection setup for Percona Platform.
+// Package saasdial provides gRPC connection setup for Percona Platform.
 package saasdial
 
 import (
