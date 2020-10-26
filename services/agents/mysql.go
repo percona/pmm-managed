ab097e76e1fc350ed9e51d5ecdda68e6051dc1d2
Andrii Skomorokhov <andrii.skomorokhov@percona.com>
2020-10-26 14:11:07 +0200

    PMM-6744 Dissable file instances collector

--- services/agents/mysql.go
+++ services/agents/mysql.go
@@ -47,7 +47,7 @@ func mysqldExporterConfig(service *models.Service, exporter *models.Agent, redac
 		"--collect.info_schema.innodb_tablespaces",
 		"--collect.info_schema.userstats",
 		"--collect.perf_schema.eventsstatements",
-		"--collect.perf_schema.file_instances",
+		// "--collect.perf_schema.file_instances",
 		"--collect.custom_query.lr",
 
 		// MR
@@ -88,6 +88,7 @@ func mysqldExporterConfig(service *models.Service, exporter *models.Agent, redac
 			"--collect.info_schema.tablestats",
 			"--collect.perf_schema.indexiowaits",
 			"--collect.perf_schema.tableiowaits",
+			"--collect.perf_schema.file_instances",
 
 			// MR
 			"--collect.perf_schema.tablelocks",
