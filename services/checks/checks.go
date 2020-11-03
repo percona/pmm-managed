d12d61e17a8f0f4910373c18a36ce3378ab20ba3
Artem Gavrilov <artem.gavrilov@percona.com>
2020-11-03 16:45:39 +0200

    SAAS-375 Add new platform public key

--- services/checks/checks.go
+++ services/checks/checks.go
@@ -81,6 +81,7 @@ var (
 
 var defaultPublicKeys = []string{
 	"RWTfyQTP3R7VzZggYY7dzuCbuCQWqTiGCqOvWRRAMVEiw0eSxHMVBBE5", // PMM 2.6
+	"RWTkF7Snv08FCboTne4djQfN5qbrLfAjb8SY3/wwEP+X5nUrkxCEvUDJ", // PMM 2.12
 }
 
 // Service is responsible for interactions with Percona Check service.
