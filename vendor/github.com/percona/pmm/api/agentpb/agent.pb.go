44db3d29a76781c236db4652cc07c5b7cb584ba8
Jiří Čtvrtka <jiri.ctvrtka.sde@gmail.com>
2020-11-05 10:45:24 +0100

    PMM-4172 Fix build.

--- vendor/github.com/percona/pmm/api/agentpb/agent.pb.go
+++ vendor/github.com/percona/pmm/api/agentpb/agent.pb.go
@@ -8,9 +8,6 @@ package agentpb
 
 import (
 	context "context"
-	reflect "reflect"
-	sync "sync"
-
 	proto "github.com/golang/protobuf/proto"
 	duration "github.com/golang/protobuf/ptypes/duration"
 	timestamp "github.com/golang/protobuf/ptypes/timestamp"
@@ -20,6 +17,8 @@ import (
 	status "google.golang.org/grpc/status"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
+	reflect "reflect"
+	sync "sync"
 )
 
 const (
