f327f9c8d289b231c3570dca6475f0b1a41273f1
Jiří Čtvrtka <jiri.ctvrtka.sde@gmail.com>
2020-11-27 14:44:07 +0100

    PMM-5364 Deps.

--- vendor/github.com/percona/pmm/api/managementpb/actions.validator.pb.go
+++ vendor/github.com/percona/pmm/api/managementpb/actions.validator.pb.go
@@ -122,12 +122,20 @@ func (this *StartPostgreSQLShowIndexActionRequest) Validate() error {
 func (this *StartPostgreSQLShowIndexActionResponse) Validate() error {
 	return nil
 }
+func (this *ServicesTLSKeys) Validate() error {
+	return nil
+}
 func (this *StartMongoDBExplainActionRequest) Validate() error {
 	if this.ServiceId == "" {
 		return github_com_mwitkow_go_proto_validators.FieldError("ServiceId", fmt.Errorf(`value '%v' must not be an empty string`, this.ServiceId))
 	}
 	if this.Query == "" {
 		return github_com_mwitkow_go_proto_validators.FieldError("Query", fmt.Errorf(`value '%v' must not be an empty string`, this.Query))
+	}
+	if this.ServicesTlsKeys != nil {
+		if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(this.ServicesTlsKeys); err != nil {
+			return github_com_mwitkow_go_proto_validators.FieldError("ServicesTlsKeys", err)
+		}
 	}
 	return nil
 }
