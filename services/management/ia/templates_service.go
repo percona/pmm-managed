b60f64e3dad27fd13aa64ce5f4e877a5c794b222
Palash Nigam <palash.nigam@percona.com>
2020-12-07 18:30:34 +0530

    Revert "PMM-7024 refactor CreateDataDir"
    This reverts commit b11c7647c3d27b88000f1120ec4c2277dbbf02bc.

--- services/management/ia/templates_service.go
+++ services/management/ia/templates_service.go
@@ -27,7 +27,6 @@ import (
 	"sort"
 	"strings"
 	"sync"
-	"syscall"
 	"time"
 
 	"github.com/golang/protobuf/ptypes"
@@ -78,27 +77,16 @@ type TemplatesService struct {
 func NewTemplatesService(db *reform.DB) *TemplatesService {
 	l := logrus.WithField("component", "management/ia/templates")
 
-	// we want owner and group of our directories to be the same as that of prometheusDir.
-	chownDirStat, err := os.Stat(prometheusDir)
-	if err != nil {
-		l.Errorf("cannot get stat of %q: %v", prometheusDir, err)
-	}
-
-	chownDirSysStat := chownDirStat.Sys().(*syscall.Stat_t)
-	uID, gID := int(chownDirSysStat.Uid), int(chownDirSysStat.Gid)
-
 	params := []dir.Params{{
-		Path:  templatesDir,
-		Perm:  dirPerm,
-		Chown: true,
-		UID:   uID,
-		GID:   gID,
+		Path:      templatesDir,
+		Perm:      dirPerm,
+		Chown:     true,
+		ChownPath: prometheusDir,
 	}, {
-		Path:  rulesDir,
-		Perm:  dirPerm,
-		Chown: true,
-		UID:   uID,
-		GID:   gID,
+		Path:      rulesDir,
+		Perm:      dirPerm,
+		Chown:     true,
+		ChownPath: prometheusDir,
 	}}
 
 	for _, p := range params {
--- utils/dir/dir.go
+++ utils/dir/dir.go
@@ -24,11 +24,10 @@ import (
 
 // Params represent the input for CreateDataDir
 type Params struct {
-	Path  string
-	Perm  os.FileMode
-	Chown bool
-	UID   int
-	GID   int
+	Path      string
+	Perm      os.FileMode
+	Chown     bool
+	ChownPath string
 }
 
 // CreateDataDir creates/updates directories with the given permissions in the persistent volume.
@@ -52,10 +51,17 @@ func CreateDataDir(params Params) error {
 
 	if params.Chown {
 		dataDirSysStat := dataDirStat.Sys().(*syscall.Stat_t)
-		uID, gID := int(dataDirSysStat.Uid), int(dataDirSysStat.Gid)
+		aUID, aGID := int(dataDirSysStat.Uid), int(dataDirSysStat.Gid)
 
-		if uID != params.UID || gID != params.GID {
-			if err := os.Chown(params.Path, params.UID, params.GID); err != nil {
+		chownDirStat, err := os.Stat(params.ChownPath)
+		if err != nil {
+			return fmt.Errorf("cannot get stat of %q: %v", params.ChownPath, err)
+		}
+
+		chownDirSysStat := chownDirStat.Sys().(*syscall.Stat_t)
+		bUID, bGID := int(chownDirSysStat.Uid), int(chownDirSysStat.Gid)
+		if aUID != bUID || aGID != bGID {
+			if err := os.Chown(params.Path, bUID, bGID); err != nil {
 				return fmt.Errorf("cannot chown datadir %v", err)
 			}
 		}
