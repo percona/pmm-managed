86574f69abcf0487a28abf0e84754d7e16a07191
Palash Nigam <palash.nigam@percona.com>
2020-12-23 16:56:24 +0530

    PMM-6964 Use subtests

--- services/alertmanager/alertmanager_test.go
+++ services/alertmanager/alertmanager_test.go
@@ -48,115 +48,137 @@ func TestAlertmanager(t *testing.T) {
 }
 
 func TestPopulateConfig(t *testing.T) {
-	var cfg alertmanager.Config
-	buf, err := ioutil.ReadFile(alertmanagerBaseConfigPath)
-	require.NoError(t, err)
-	err = yaml.Unmarshal(buf, &cfg)
-	require.NoError(t, err)
+	t.Run("without receivers and routes", func(t *testing.T) {
+		var cfg alertmanager.Config
+		buf, err := ioutil.ReadFile(alertmanagerBaseConfigPath)
+		require.NoError(t, err)
+		err = yaml.Unmarshal(buf, &cfg)
+		require.NoError(t, err)
 
-	sqlDB := testdb.Open(t, models.SkipFixtures, nil)
-	db := reform.NewDB(sqlDB, postgresql.Dialect, reform.NewPrintfLogger(t.Logf))
+		sqlDB := testdb.Open(t, models.SkipFixtures, nil)
+		db := reform.NewDB(sqlDB, postgresql.Dialect, reform.NewPrintfLogger(t.Logf))
+		svc := New(db)
+		err = svc.populateConfig(&cfg)
+		require.NoError(t, err)
 
-	channel1, err := models.CreateChannel(db.Querier, &models.CreateChannelParams{
-		Summary: "some summary",
-		EmailConfig: &models.EmailConfig{
-			To: []string{"test@test.test"},
-		},
-		Disabled: false,
+		assert.Len(t, cfg.Receivers, 1)
+		assert.Equal(t, "empty", cfg.Receivers[0].Name)
+		assert.Equal(t, "empty", cfg.Route.Receiver)
+		assert.Len(t, cfg.Route.Routes, 0)
 	})
-	require.NoError(t, err)
 
-	channel2, err := models.CreateChannel(db.Querier, &models.CreateChannelParams{
-		Summary: "some summary",
-		PagerDutyConfig: &models.PagerDutyConfig{
-			RoutingKey: "ms-pagerduty-dev",
-		},
-		Disabled: false,
-	})
-	require.NoError(t, err)
+	t.Run("with receivers and routes", func(t *testing.T) {
+		// use the same config as base file
+		var cfg alertmanager.Config
+		buf, err := ioutil.ReadFile(alertmanagerBaseConfigPath)
+		require.NoError(t, err)
+		err = yaml.Unmarshal(buf, &cfg)
+		require.NoError(t, err)
 
-	templateName := gofakeit.UUID()
-	_, err = models.CreateTemplate(db.Querier, &models.CreateTemplateParams{
-		Template: &alert.Template{
-			Name:    templateName,
-			Version: 1,
-			Summary: gofakeit.Quote(),
-			Tiers:   []common.Tier{common.Anonymous},
-			Expr:    gofakeit.Quote(),
-			Params: []alert.Parameter{{
-				Name:    gofakeit.UUID(),
-				Summary: gofakeit.Quote(),
-				Unit:    gofakeit.Letter(),
-				Type:    alert.Float,
-				Range:   []interface{}{float64(10), float64(100)},
-				Value:   float64(50),
-			}},
-			For:         3,
-			Severity:    common.Warning,
-			Labels:      map[string]string{"foo": "bar"},
-			Annotations: nil,
-		},
-		Source: "USER_FILE",
-	})
-	require.NoError(t, err)
+		sqlDB := testdb.Open(t, models.SkipFixtures, nil)
+		db := reform.NewDB(sqlDB, postgresql.Dialect, reform.NewPrintfLogger(t.Logf))
 
-	rule, err := models.CreateRule(db.Querier, &models.CreateRuleParams{
-		TemplateName: templateName,
-		Disabled:     true,
-		RuleParams: []models.RuleParam{
-			{
-				Name:       "test",
-				Type:       models.Float,
-				FloatValue: 3.14,
+		channel1, err := models.CreateChannel(db.Querier, &models.CreateChannelParams{
+			Summary: "some summary",
+			EmailConfig: &models.EmailConfig{
+				To: []string{"test@test.test"},
 			},
-		},
-		For:          5 * time.Second,
-		Severity:     common.Warning,
-		CustomLabels: map[string]string{"foo": "bar"},
-		Filters:      []models.Filter{{Type: models.Equal, Key: "value", Val: "10"}},
-		ChannelIDs:   []string{channel1.ID, channel2.ID},
+			Disabled: false,
+		})
+		require.NoError(t, err)
+
+		channel2, err := models.CreateChannel(db.Querier, &models.CreateChannelParams{
+			Summary: "some summary",
+			PagerDutyConfig: &models.PagerDutyConfig{
+				RoutingKey: "ms-pagerduty-dev",
+			},
+			Disabled: false,
+		})
+		require.NoError(t, err)
+
+		templateName := gofakeit.UUID()
+		_, err = models.CreateTemplate(db.Querier, &models.CreateTemplateParams{
+			Template: &alert.Template{
+				Name:    templateName,
+				Version: 1,
+				Summary: gofakeit.Quote(),
+				Tiers:   []common.Tier{common.Anonymous},
+				Expr:    gofakeit.Quote(),
+				Params: []alert.Parameter{{
+					Name:    gofakeit.UUID(),
+					Summary: gofakeit.Quote(),
+					Unit:    gofakeit.Letter(),
+					Type:    alert.Float,
+					Range:   []interface{}{float64(10), float64(100)},
+					Value:   float64(50),
+				}},
+				For:         3,
+				Severity:    common.Warning,
+				Labels:      map[string]string{"foo": "bar"},
+				Annotations: nil,
+			},
+			Source: "USER_FILE",
+		})
+		require.NoError(t, err)
+
+		rule, err := models.CreateRule(db.Querier, &models.CreateRuleParams{
+			TemplateName: templateName,
+			Disabled:     true,
+			RuleParams: []models.RuleParam{
+				{
+					Name:       "test",
+					Type:       models.Float,
+					FloatValue: 3.14,
+				},
+			},
+			For:          5 * time.Second,
+			Severity:     common.Warning,
+			CustomLabels: map[string]string{"foo": "bar"},
+			Filters:      []models.Filter{{Type: models.Equal, Key: "value", Val: "10"}},
+			ChannelIDs:   []string{channel1.ID, channel2.ID},
+		})
+		require.NoError(t, err)
+
+		settings, err := models.UpdateSettings(db.Querier, &models.ChangeSettingsParams{
+			EmailAlertingSettings: &models.EmailAlertingSettings{
+				From:      tests.GenEmail(t),
+				Smarthost: "0.0.0.0:80",
+				Hello:     "host",
+				Username:  "user",
+				Password:  "password",
+				Identity:  "id",
+				Secret:    "secret",
+			},
+			SlackAlertingSettings: &models.SlackAlertingSettings{
+				URL: gofakeit.URL(),
+			},
+		})
+		require.NoError(t, err)
+
+		svc := New(db)
+		err = svc.populateConfig(&cfg)
+		require.NoError(t, err)
+
+		assert.Len(t, cfg.Receivers, 2)
+		assert.Equal(t, "empty", cfg.Receivers[0].Name)
+		assert.Equal(t, channel1.ID+receiverNameSeparator+channel2.ID, cfg.Receivers[1].Name)
+		assert.NotNil(t, cfg.Receivers[1].EmailConfigs)
+		assert.NotNil(t, cfg.Receivers[1].PagerdutyConfigs)
+		assert.Equal(t, "empty", cfg.Route.Receiver)
+		assert.Len(t, cfg.Route.Routes, 1)
+		assert.Equal(t, rule.ID, cfg.Route.Routes[0].Match["rule_id"])
+		// check global config
+		assert.Equal(t, cfg.Global.SMTPFrom, settings.IntegratedAlerting.EmailAlertingSettings.From)
+		assert.Equal(t, cfg.Global.SMTPHello, settings.IntegratedAlerting.EmailAlertingSettings.Hello)
+		assert.Equal(t, cfg.Global.SMTPAuthUsername, settings.IntegratedAlerting.EmailAlertingSettings.Username)
+		assert.Equal(t, cfg.Global.SMTPAuthPassword, settings.IntegratedAlerting.EmailAlertingSettings.Password)
+		assert.Equal(t, cfg.Global.SMTPAuthIdentity, settings.IntegratedAlerting.EmailAlertingSettings.Identity)
+		assert.Equal(t, cfg.Global.SMTPAuthSecret, settings.IntegratedAlerting.EmailAlertingSettings.Secret)
+
+		host, port, err := net.SplitHostPort(settings.IntegratedAlerting.EmailAlertingSettings.Smarthost)
+		require.NoError(t, err)
+		assert.Equal(t, cfg.Global.SMTPSmarthost.Host, host)
+		assert.Equal(t, cfg.Global.SMTPSmarthost.Port, port)
+		assert.Equal(t, cfg.Global.SlackAPIURL, settings.IntegratedAlerting.SlackAlertingSettings.URL)
 	})
-	require.NoError(t, err)
-
-	settings, err := models.UpdateSettings(db.Querier, &models.ChangeSettingsParams{
-		EmailAlertingSettings: &models.EmailAlertingSettings{
-			From:      tests.GenEmail(t),
-			Smarthost: "0.0.0.0:80",
-			Hello:     "host",
-			Username:  "user",
-			Password:  "password",
-			Identity:  "id",
-			Secret:    "secret",
-		},
-		SlackAlertingSettings: &models.SlackAlertingSettings{
-			URL: gofakeit.URL(),
-		},
-	})
-	require.NoError(t, err)
-
-	svc := New(db)
-	err = svc.populateConfig(&cfg)
-	require.NoError(t, err)
-
-	assert.Len(t, cfg.Receivers, 2)
-	assert.Equal(t, "empty", cfg.Receivers[0].Name)
-	assert.Equal(t, channel1.ID+receiverNameSeparator+channel2.ID, cfg.Receivers[1].Name)
-	assert.NotNil(t, cfg.Receivers[1].EmailConfigs)
-	assert.NotNil(t, cfg.Receivers[1].PagerdutyConfigs)
-	assert.Equal(t, "empty", cfg.Route.Receiver)
-	assert.Len(t, cfg.Route.Routes, 1)
-	assert.Equal(t, rule.ID, cfg.Route.Routes[0].Match["rule_id"])
-	// check global config
-	assert.Equal(t, cfg.Global.SMTPFrom, settings.IntegratedAlerting.EmailAlertingSettings.From)
-	assert.Equal(t, cfg.Global.SMTPHello, settings.IntegratedAlerting.EmailAlertingSettings.Hello)
-	assert.Equal(t, cfg.Global.SMTPAuthUsername, settings.IntegratedAlerting.EmailAlertingSettings.Username)
-	assert.Equal(t, cfg.Global.SMTPAuthPassword, settings.IntegratedAlerting.EmailAlertingSettings.Password)
-	assert.Equal(t, cfg.Global.SMTPAuthIdentity, settings.IntegratedAlerting.EmailAlertingSettings.Identity)
-	assert.Equal(t, cfg.Global.SMTPAuthSecret, settings.IntegratedAlerting.EmailAlertingSettings.Secret)
-
-	host, port, err := net.SplitHostPort(settings.IntegratedAlerting.EmailAlertingSettings.Smarthost)
-	require.NoError(t, err)
-	assert.Equal(t, cfg.Global.SMTPSmarthost.Host, host)
-	assert.Equal(t, cfg.Global.SMTPSmarthost.Port, port)
-	assert.Equal(t, cfg.Global.SlackAPIURL, settings.IntegratedAlerting.SlackAlertingSettings.URL)
 }
