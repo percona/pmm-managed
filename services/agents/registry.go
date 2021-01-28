d73a550fd8571dd8318d1fda8e1db5134ff74dfd
Nurlan Moldomurov <nurlan.moldomurov@percona.com>
2021-01-28 17:29:52 +0300

    PMM-7426 Fix state change stuck.

--- services/agents/registry.go
+++ services/agents/registry.go
@@ -47,15 +47,21 @@ const (
 	prometheusSubsystem = "agents"
 )
 
+const (
+	updateBatchDelay   = time.Second
+	stateChangeTimeout = 5 * time.Second
+)
+
 var (
 	defaultActionTimeout      = ptypes.DurationProto(10 * time.Second)
 	defaultQueryActionTimeout = ptypes.DurationProto(15 * time.Second) // should be less than checks.resultTimeout
 )
 
 type pmmAgentInfo struct {
-	channel *channel.Channel
-	id      string
-	kick    chan struct{}
+	channel         *channel.Channel
+	id              string
+	stateChangeChan chan struct{}
+	kick            chan struct{}
 }
 
 // Registry keeps track of all connected pmm-agents.
@@ -148,6 +154,8 @@ func (r *Registry) Run(stream agentpb.Agent_ConnectServer) error {
 	defer func() {
 		l.Infof("Disconnecting client: %s.", disconnectReason)
 	}()
+
+	go r.RunStateChangeHandler(ctx, agent)
 
 	// send first SetStateRequest concurrently with handling ping from agent
 	go r.SendSetStateRequest(ctx, agent.id)
@@ -269,9 +277,10 @@ func (r *Registry) register(stream agentpb.Agent_ConnectServer) (*pmmAgentInfo, 
 	}
 
 	agent := &pmmAgentInfo{
-		channel: channel.New(stream, r.sharedMetrics),
-		id:      agentMD.ID,
-		kick:    make(chan struct{}),
+		channel:         channel.New(stream, r.sharedMetrics),
+		id:              agentMD.ID,
+		stateChangeChan: make(chan struct{}, 1),
+		kick:            make(chan struct{}),
 	}
 	r.agents[agentMD.ID] = agent
 	return agent, nil
@@ -397,6 +406,7 @@ func (r *Registry) Kick(ctx context.Context, pmmAgentID string) {
 	delete(r.agents, pmmAgentID)
 	r.roster.clear(pmmAgentID)
 	close(agent.kick)
+	close(agent.stateChangeChan)
 }
 
 // ping sends Ping message to given Agent, waits for Pong and observes round-trip time and clock drift.
@@ -504,15 +514,47 @@ func (r *Registry) UpdateAgentsState(ctx context.Context) error {
 	return nil
 }
 
+func (r *Registry) RunStateChangeHandler(ctx context.Context, agent *pmmAgentInfo) {
+	l := logger.Get(ctx).WithField("agent_id", agent.id)
+
+	l.Info("Starting RunStateChangeHandler ...")
+	defer l.Info("Done RunStateChangeHandler.")
+
+	// stateChangeChan, state update loop, and SendSetStateRequest method ensure that state
+	// is reloaded when requested, but several requests are batched together to avoid too often reloads.
+	// That allows the caller to just call SendSetStateRequest when it seems fit.
+	if cap(agent.stateChangeChan) != 1 {
+		panic("stateChangeChan should have capacity 1")
+	}
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+
+		case <-agent.kick:
+			return
+
+		case <-agent.stateChangeChan:
+			// batch several update requests together by delaying the first one
+			sleepCtx, sleepCancel := context.WithTimeout(ctx, updateBatchDelay)
+			<-sleepCtx.Done()
+			sleepCancel()
+
+			if ctx.Err() != nil {
+				return
+			}
+
+			nCtx, cancel := context.WithTimeout(ctx, stateChangeTimeout)
+			r.sendSetStateRequest(nCtx, agent)
+			cancel()
+		}
+	}
+}
+
 // SendSetStateRequest sends SetStateRequest to pmm-agent with given ID.
 func (r *Registry) SendSetStateRequest(ctx context.Context, pmmAgentID string) {
 	l := logger.Get(ctx)
-	start := time.Now()
-	defer func() {
-		if dur := time.Since(start); dur > time.Second {
-			l.Warnf("SendSetStateRequest took %s.", dur)
-		}
-	}()
 
 	agent, err := r.get(pmmAgentID)
 	if err != nil {
@@ -520,7 +562,21 @@ func (r *Registry) SendSetStateRequest(ctx context.Context, pmmAgentID string) {
 		return
 	}
 
-	pmmAgent, err := models.FindAgentByID(r.db.Querier, pmmAgentID)
+	select {
+	case agent.stateChangeChan <- struct{}{}:
+	default:
+	}
+}
+
+func (r *Registry) sendSetStateRequest(ctx context.Context, agent *pmmAgentInfo) {
+	l := logger.Get(ctx)
+	start := time.Now()
+	defer func() {
+		if dur := time.Since(start); dur > time.Second {
+			l.Warnf("SendSetStateRequest took %s.", dur)
+		}
+	}()
+	pmmAgent, err := models.FindAgentByID(r.db.Querier, agent.id)
 	if err != nil {
 		l.Errorf("Failed to get PMM Agent: %s.", err)
 		return
@@ -531,7 +587,7 @@ func (r *Registry) SendSetStateRequest(ctx context.Context, pmmAgentID string) {
 		return
 	}
 
-	agents, err := models.FindAgents(r.db.Querier, models.AgentFilters{PMMAgentID: pmmAgentID})
+	agents, err := models.FindAgents(r.db.Querier, models.AgentFilters{PMMAgentID: agent.id})
 	if err != nil {
 		l.Errorf("Failed to collect agents: %s.", err)
 		return
@@ -555,9 +611,9 @@ func (r *Registry) SendSetStateRequest(ctx context.Context, pmmAgentID string) {
 		case models.PMMAgentType:
 			continue
 		case models.VMAgentType:
-			scrapeCfg, err := r.vmdb.BuildScrapeConfigForVMAgent(pmmAgentID)
+			scrapeCfg, err := r.vmdb.BuildScrapeConfigForVMAgent(agent.id)
 			if err != nil {
-				l.WithError(err).Errorf("cannot get agent scrape config for agent: %s", pmmAgentID)
+				l.WithError(err).Errorf("cannot get agent scrape config for agent: %s", agent.id)
 			}
 			agentProcesses[row.AgentID] = vmAgentConfig(string(scrapeCfg))
 
@@ -623,7 +679,7 @@ func (r *Registry) SendSetStateRequest(ctx context.Context, pmmAgentID string) {
 		}
 		sort.Strings(rdsExporterIDs)
 
-		groupID := r.roster.add(pmmAgentID, rdsGroup, rdsExporterIDs)
+		groupID := r.roster.add(agent.id, rdsGroup, rdsExporterIDs)
 		c, err := rdsExporterConfig(rdsExporters, redactMode)
 		if err == nil {
 			agentProcesses[groupID] = c
