710241917f2c9a357c8dfd3e59047af23709b83c
Jiri Ctvrtka <jiri.ctvrtka.sde@gmail.com>
2021-02-01 08:34:14 +0100

    PMM-6640 Deps.

--- vendor/github.com/percona/pmm/api/inventorypb/services.validator.pb.go
+++ vendor/github.com/percona/pmm/api/inventorypb/services.validator.pb.go
@@ -37,6 +37,10 @@ func (this *ExternalService) Validate() error {
 	// Validation of proto3 map<> fields is unsupported.
 	return nil
 }
+func (this *HAProxyService) Validate() error {
+	// Validation of proto3 map<> fields is unsupported.
+	return nil
+}
 func (this *ListServicesRequest) Validate() error {
 	return nil
 }
@@ -73,6 +77,13 @@ func (this *ListServicesResponse) Validate() error {
 		if item != nil {
 			if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(item); err != nil {
 				return github_com_mwitkow_go_proto_validators.FieldError("External", err)
+			}
+		}
+	}
+	for _, item := range this.Haproxy {
+		if item != nil {
+			if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(item); err != nil {
+				return github_com_mwitkow_go_proto_validators.FieldError("Haproxy", err)
 			}
 		}
 	}
@@ -117,6 +128,13 @@ func (this *GetServiceResponse) Validate() error {
 		if oneOfNester.External != nil {
 			if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(oneOfNester.External); err != nil {
 				return github_com_mwitkow_go_proto_validators.FieldError("External", err)
+			}
+		}
+	}
+	if oneOfNester, ok := this.GetService().(*GetServiceResponse_Haproxy); ok {
+		if oneOfNester.Haproxy != nil {
+			if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(oneOfNester.Haproxy); err != nil {
+				return github_com_mwitkow_go_proto_validators.FieldError("Haproxy", err)
 			}
 		}
 	}
@@ -205,6 +223,24 @@ func (this *AddExternalServiceRequest) Validate() error {
 	return nil
 }
 func (this *AddExternalServiceResponse) Validate() error {
+	if this.External != nil {
+		if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(this.External); err != nil {
+			return github_com_mwitkow_go_proto_validators.FieldError("External", err)
+		}
+	}
+	return nil
+}
+func (this *AddHAProxyServiceRequest) Validate() error {
+	if this.ServiceName == "" {
+		return github_com_mwitkow_go_proto_validators.FieldError("ServiceName", fmt.Errorf(`value '%v' must not be an empty string`, this.ServiceName))
+	}
+	if this.NodeId == "" {
+		return github_com_mwitkow_go_proto_validators.FieldError("NodeId", fmt.Errorf(`value '%v' must not be an empty string`, this.NodeId))
+	}
+	// Validation of proto3 map<> fields is unsupported.
+	return nil
+}
+func (this *AddHAProxyServiceResponse) Validate() error {
 	if this.External != nil {
 		if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(this.External); err != nil {
 			return github_com_mwitkow_go_proto_validators.FieldError("External", err)
