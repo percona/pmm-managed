a595416816228fd888c7dfb77eba5aa923973028
Jaroslav Musil <jaroslav.musil.sde@gmail.com>
2021-02-16 11:13:54 +0100

    PMM-7232 Updated mock.

--- services/server/deps.go
+++ services/server/deps.go
@@ -35,6 +35,7 @@ import (
 //go:generate mockery -name=telemetryService -case=snake -inpkg -testonly
 //go:generate mockery -name=platformService -case=snake -inpkg -testonly
 //go:generate mockery -name=agentsRegistry -case=snake -inpkg -testonly
+//go:generate mockery -name=rulesService -case=snake -inpkg -testonly
 
 // healthChecker interface wraps all services that implements the IsReady method to report the
 // service health for the Readiness check.
