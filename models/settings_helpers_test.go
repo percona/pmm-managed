30c7712b6634f2a8e011c5878903b6de34cf7450
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2021-02-25 10:40:40 +0100

    PMM-7337 update settings helpers tests

--- models/settings_helpers_test.go
+++ models/settings_helpers_test.go
@@ -47,6 +47,9 @@ func TestSettings(t *testing.T) {
 			DataRetention: 30 * 24 * time.Hour,
 			AWSPartitions: []string{"aws"},
 		}
+		expected.SaaS.STTCheckIntervals.DefaultInterval = 86400 * time.Second
+		expected.SaaS.STTCheckIntervals.RareInterval = 86400 * time.Second
+		expected.SaaS.STTCheckIntervals.FrequentInterval = 86400 * time.Second
 		assert.Equal(t, expected, actual)
 	})
 
@@ -63,6 +66,9 @@ func TestSettings(t *testing.T) {
 			DataRetention: 30 * 24 * time.Hour,
 			AWSPartitions: []string{"aws"},
 		}
+		expected.SaaS.STTCheckIntervals.DefaultInterval = 86400 * time.Second
+		expected.SaaS.STTCheckIntervals.RareInterval = 86400 * time.Second
+		expected.SaaS.STTCheckIntervals.FrequentInterval = 86400 * time.Second
 		assert.Equal(t, expected, s)
 	})
 
