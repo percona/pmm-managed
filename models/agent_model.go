9b3759f4d8e0202d763c7b128619667640909aac
Jiri Ctvrtka <jiri.ctvrtka.sde@gmail.com>
2021-03-17 14:56:17 +0100

    PMM-7676 Fix.

--- models/agent_model.go
+++ models/agent_model.go
@@ -107,10 +107,10 @@ type Agent struct {
 	AWSAccessKey *string `reform:"aws_access_key"`
 	AWSSecretKey *string `reform:"aws_secret_key"`
 
-	AzureDatabaseClientID       *string `reform:"azure_client_id"`
-	AzureDatabaseClientSecret   *string `reform:"azure_client_secret"`
-	AzureDatabaseTenantID       *string `reform:"azure_tenant_id"`
-	AzureDatabaseSubscriptionID *string `reform:"azure_subscription_id"`
+	AzureDatabaseClientID       *string `reform:"azure_database_client_id"`
+	AzureDatabaseClientSecret   *string `reform:"azure_database_client_secret"`
+	AzureDatabaseTenantID       *string `reform:"azure_database_tenant_id"`
+	AzureDatabaseSubscriptionID *string `reform:"azure_database_subscription_id"`
 
 	// TableCount stores last known table count. NULL if unknown.
 	TableCount *int32 `reform:"table_count"`
