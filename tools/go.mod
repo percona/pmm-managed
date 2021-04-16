bea84c3b30be917e07e9c9310d035c2da4af9932
Dasio <david.mikus@sde.cz>
2021-04-16 18:26:13 +0200

    Go mod tidy and licence check

--- tools/go.mod
+++ tools/go.mod
@@ -3,7 +3,11 @@ module github.com/percona/pmm-managed/tools
 go 1.16
 
 require (
-	github.com/kevinburke/go-bindata v3.22.0+incompatible // indirect
-	github.com/vektra/mockery v1.1.2 // indirect
-	gopkg.in/reform.v1 v1.5.0 // indirect
+	github.com/golangci/golangci-lint v1.39.0
+	github.com/kevinburke/go-bindata v3.22.0+incompatible
+	github.com/reviewdog/reviewdog v0.11.0
+	github.com/vektra/mockery v1.1.2
+	golang.org/x/perf v0.0.0-20210220033136-40a54f11e909
+	golang.org/x/tools v0.1.0
+	gopkg.in/reform.v1 v1.5.0
 )
--- tools/tools.go
+++ tools/tools.go
@@ -1,17 +1,18 @@
 // pmm-managed
-// Copyright 2019 Percona LLC
+// Copyright (C) 2017 Percona LLC
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
-//  http://www.apache.org/licenses/LICENSE-2.0
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 //go:build tools
 // +build tools
@@ -26,9 +27,6 @@ import (
 
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/reviewdog/reviewdog/cmd/reviewdog"
-	_ "github.com/vektra/mockery/cmd/mockery"
 	_ "golang.org/x/perf/cmd/benchstat"
-	_ "golang.org/x/tools/cmd/goimports"
-	_ "gopkg.in/reform.v1/reform"
 	_ "gopkg.in/reform.v1/reform-db"
 )
