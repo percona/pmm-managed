6805ad0fb3678ce418473deab6ff7bb8a30a1a12
Vadim Yalovets <vadim.yalovets@percona.com>
2021-04-17 13:34:24 +0300

    SAAS-529-two-types-checks

--- services/checks/funcs.go
+++ services/checks/funcs.go
@@ -88,15 +88,15 @@ func formatVersionNum(args ...interface{}) (interface{}, error) {
 		}
 
 		return p.String(), nil
-	} else {
-		p := &version.Parsed{
-			Major: int(num / 10000),
-			Minor: int(num / 100 % 100),
-			Patch: int(num % 100),
-		}
-
-		return p.String(), nil
 	}
+
+	p := &version.Parsed{
+		Major: int(num / 10000),
+		Minor: int(num / 100 % 100),
+		Patch: int(num % 100),
+	}
+
+	return p.String(), nil
 }
 
 // GetAdditionalContext returns additional functions to be used in check scripts.
