d067b2dff942d878474628250947b9a8f91fe368
Jiri Ctvrtka <jiri.ctvrtka.sde@gmail.com>
2021-04-23 11:46:48 +0200

    PMM-7847 Format.

--- services/agents/mysql.go
+++ services/agents/mysql.go
@@ -102,9 +102,9 @@ func mysqldExporterConfig(service *models.Service, exporter *models.Agent, redac
 		for k := range files {
 			switch k {
 			case "tlsCert":
-				args = append(args, "--mysql.ssl-cert-file=" + tdp.Left + " .TextFiles.tlsCert " + tdp.Right)
+				args = append(args, "--mysql.ssl-cert-file="+tdp.Left+" .TextFiles.tlsCert "+tdp.Right)
 			case "tlsKey":
-				args = append(args, "--mysql.ssl-key-file=" + tdp.Left + " .TextFiles.tlsKey " + tdp.Right)
+				args = append(args, "--mysql.ssl-key-file="+tdp.Left+" .TextFiles.tlsKey "+tdp.Right)
 			default:
 				continue
 			}
