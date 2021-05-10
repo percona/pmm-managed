e065ac531c5be310fcad5fdc7f859ad8722eee57
Jan Prukner <jan.prukner.sde@gmail.com>
2021-05-10 08:54:49 +0200

    change back the timeout to 3s

--- services/agents/channel/channel_test.go
+++ services/agents/channel/channel_test.go
@@ -76,7 +76,7 @@ func setup(t *testing.T, connect func(*Channel) error, expected ...error) (agent
 		require.NoError(t, err)
 	}()
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 
 	// make client and channel
 	opts := []grpc.DialOption{
