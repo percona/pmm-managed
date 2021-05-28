0999b64637ae5bec27b0e1a58c5abe706ea3ed4b
Jan Prukner <jan.prukner.sde@gmail.com>
2021-05-28 18:27:03 +0200

    Revert "update deps"
    This reverts commit d7732029fb82ef7141b2a047079075227639fd65.

--- models/agent_model.go
+++ models/agent_model.go
@@ -41,7 +41,7 @@ type AgentType string
 const (
 	certificateKeyFilePlaceholder = "certificateKeyFilePlaceholder"
 	caFilePlaceholder             = "caFilePlaceholder"
-	agentStatusUnknown            = "UNKNOWN"
+	AgentStatusUnknown            = "UNKNOWN"
 )
 
 // Agent types (in the same order as in agents.proto).
@@ -172,7 +172,7 @@ func (s *Agent) BeforeInsert() error {
 		s.CustomLabels = nil
 	}
 	if s.Status == "" && s.AgentType != ExternalExporterType && s.AgentType != PMMAgentType {
-		s.Status = agentStatusUnknown
+		s.Status = AgentStatusUnknown
 	}
 	return nil
 }
