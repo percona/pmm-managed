c515f86876106eec8543a008efbc2b327d9b07ba
Dasio <david.mikus@sde.cz>
2021-06-23 12:39:34 +0200

    PMM-8191 License headers

--- services/scheduler/task.go
+++ services/scheduler/task.go
@@ -1,3 +1,19 @@
+// pmm-managed
+// Copyright (C) 2017 Percona LLC
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 package scheduler
 
 import (
@@ -44,15 +60,18 @@ func NewPrintTask(message string) *PrintTask {
 	}
 }
 
+// Run starts task.
 func (j *PrintTask) Run(ctx context.Context) error {
 	logrus.Info(j.Message)
 	return nil
 }
 
+// Type returns task type.
 func (j *PrintTask) Type() models.ScheduledTaskType {
 	return models.ScheduledPrintTask
 }
 
+// Data returns data needed for running a task.
 func (j *PrintTask) Data() models.ScheduledTaskData {
 	return models.ScheduledTaskData{
 		Print: &models.PrintTaskData{
