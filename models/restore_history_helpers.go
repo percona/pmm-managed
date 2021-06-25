c03168eaf26de16fcdecbcf948c9e11b146a31a2
Maksym Hilliaka <maksym.hilliaka@percona.com>
2021-06-25 13:40:13 +0300

    fix placeholder index

--- models/restore_history_helpers.go
+++ models/restore_history_helpers.go
@@ -39,19 +39,21 @@ func FindRestoreHistoryItems(q *reform.Querier, filters *RestoreHistoryItemFilte
 	var conditions []string
 	var args []interface{}
 
+	idx := 1
 	if filters != nil && filters.ServiceID != "" {
 		if _, err := FindServiceByID(q, filters.ServiceID); err != nil {
 			return nil, err
 		}
-		conditions = append(conditions, fmt.Sprintf("service_id = %s", q.Placeholder(1)))
+		conditions = append(conditions, fmt.Sprintf("service_id = %s", q.Placeholder(idx)))
 		args = append(args, filters.ServiceID)
+		idx++
 	}
 
 	if filters != nil && filters.ArtifactID != "" {
 		if _, err := FindArtifactByID(q, filters.ArtifactID); err != nil {
 			return nil, err
 		}
-		conditions = append(conditions, fmt.Sprintf("artifact_id = %s", q.Placeholder(1)))
+		conditions = append(conditions, fmt.Sprintf("artifact_id = %s", q.Placeholder(idx)))
 		args = append(args, filters.ArtifactID)
 	}
 
