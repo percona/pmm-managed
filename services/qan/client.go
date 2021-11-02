11915cc2a3b15ea6d4f24b4b1eb973d28477fd5d
Jiri Ctvrtka <jiri.ctvrtka.sde@gmail.com>
2021-11-02 12:36:49 +0100

    PMM-8588 Changes.

--- services/qan/client.go
+++ services/qan/client.go
@@ -516,5 +516,5 @@ func fillPostgreSQL(mb *qanpb.MetricsBucket, bp *agentpb.MetricsBucket_PostgreSQ
 	mb.ApplicationName = bp.ApplicationName
 	mb.Planid = bp.Planid
 	mb.QueryPlan = bp.QueryPlan
-	mb.RespCalls = bp.RespCalls
+	mb.HistogramItems = bp.HistogramItems
 }
