21bed0c3671360f5eb4adfd45567c1e8681b549c
Jan Prukner <jan.prukner@3pillarglobal.com>
2021-11-30 11:11:27 +0100

    remove changes

--- services/management/azure_database.go
+++ services/management/azure_database.go
@@ -76,7 +76,7 @@ func NewAzureDatabaseService(db *reform.DB, registry agentsRegistry, state agent
 
 // Enabled returns if service is enabled and can be used.
 func (s *AzureDatabaseService) Enabled() bool {
-	settings, err := models.GetSettings(s.db.Querier)
+	settings, err := models.GetSettings(s.db)
 	if err != nil {
 		s.l.WithError(err).Error("can't get settings")
 		return false
