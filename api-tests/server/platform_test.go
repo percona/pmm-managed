81fbc795a95fbb9792bbef53cbc6c3a5b37ea04a
Jiri Ctvrtka <jiri.ctvrtka.sde@gmail.com>
2021-12-13 10:21:03 +0100

    PMM-9228 Skip API test for now.

--- api-tests/server/platform_test.go
+++ api-tests/server/platform_test.go
@@ -124,6 +124,8 @@ func TestPlatform(t *testing.T) {
 		})
 
 		t.Run("successful connect and disconnect", func(t *testing.T) {
+			t.Skip("Skip this test until we've got disconnect")
+
 			_, err := client.Connect(&platform.ConnectParams{
 				Body: platform.ConnectBody{
 					ServerName: serverName,
